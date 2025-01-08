be26e474e01f6c1523127a8e5f6f12e6703c7aae
shamal melantha <shamalmelantha3@gmail.com>
2025-01-08 08:57:30 +0530

    update cors

--- index.js
+++ index.js
@@ -2,39 +2,34 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const dotenv = require('dotenv').config();
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-
+const dotenv= require("dotenv").config();
 const app = express();
 const port = 8000;
+const cors = require('cors');
 
-// Configure CORS
-app.use(cors({
-  origin: 'https://location-backend-five.vercel.app', 
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-}));
 
-app.get("/",(req,res)=>{
-  res.json({meesage:"hello world from backend"})
-
-})
-
-// Middleware
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Connect to MongoDB
+app.use(cors({
+  origin: ["http://localhost:3000", "https://location-tracking-admin-ten.vercel.app"],
+  credentials: true
+}));
+
+const jwt = require('jsonwebtoken');
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log("Error connecting to MongoDB", err);
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((err) => {
+  console.log("Error connecting to MongoDB", err);
+});
+
+app.listen(port, () => {
+  console.log('Server running on port 8000');
 });
 
 const User = require("./models/user");
